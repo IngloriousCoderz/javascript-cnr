1f08173d20b1524e67bab25a09f519927a3f170f
Matteo Antony Mistretta <m.mistretta@reply.it>
2024-09-26 18:00:43 +0200

    Explain arrays and strings

---
+++ 05-arrays.js
@@ -0,0 +1,114 @@
+//               0  1  2  3  4
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+// CRUD - Create, Read/Retrieve, Update, Delete
+
+// R
+const firstItem = numbers[0];
+const secondItem = numbers[1];
+const lastItem = numbers[numbers.length - 1];
+console.log(firstItem, secondItem, lastItem);
+
+// C
+numbers.push(6);
+console.log(numbers);
+
+numbers.unshift(-1);
+console.log(numbers);
+
+numbers.splice(2, 0, 8, 9);
+console.log(numbers);
+
+// D
+numbers.pop();
+console.log(numbers);
+
+numbers.shift();
+console.log(numbers);
+
+numbers.splice(1, 2);
+console.log(numbers);
+
+// U
+numbers[2] = 7;
+console.log(numbers);
+
+// Destrutturazione
+{
+  // const firstItem = numbers[0];
+  // const secondItem = numbers[1];
+  // const lastItem = numbers[numbers.length - 1];
+  const [firstItem, secondItem, , , lastItem] = numbers;
+  console.log(firstItem, secondItem, lastItem);
+
+  const vector = [2, 0, 1];
+  const [x, y, z] = vector;
+  console.log(x, y, z);
+}
+
+// Spread operator
+{
+  const [, , ...remainingItems] = numbers;
+  console.log(remainingItems);
+
+  console.log(numbers.slice(2, 4));
+  console.log(numbers.slice(2));
+  console.log(numbers.slice(0, 3));
+  console.log(numbers);
+
+  const newNumbers = ["a", "b", ...numbers, "c", "d"];
+  console.log(newNumbers);
+}
+
+// C
+{
+  const newNumbers = [...numbers, 8];
+  console.log(numbers, newNumbers);
+}
+
+{
+  const newNumbers = [8, ...numbers];
+  console.log(numbers, newNumbers);
+}
+
+{
+  const newNumbers = [
+    //
+    ...numbers.slice(0, 2),
+    8,
+    ...numbers.slice(2),
+  ];
+  console.log(numbers, newNumbers);
+}
+
+// D
+{
+  const newNumbers = numbers.slice(0, -1);
+  console.log(numbers, newNumbers);
+}
+
+{
+  const newNumbers = numbers.slice(1);
+  console.log(numbers, newNumbers);
+}
+
+{
+  const newNumbers = [
+    //
+    ...numbers.slice(0, 2),
+    ...numbers.slice(3),
+  ];
+  console.log(numbers, newNumbers);
+}
+
+// U
+{
+  const newNumbers = [
+    //
+    ...numbers.slice(0, 2),
+    3,
+    ...numbers.slice(3),
+  ];
+  console.log(numbers, newNumbers);
+}
---
+++ 06-strings.js
@@ -0,0 +1,33 @@
+const message = 'Hello "world"! <div class="text"></div>';
+const anotherMessage = "Hello 'world'! Un po' come mi pare, doesn't it?";
+const finalMessage = "'Hello' \"world\"!";
+
+const language = "JavaScript";
+const advantage =
+  "Posso interpolare espressioni " + language.toUpperCase() + "!";
+console.log(advantage);
+
+const templateLiteral = `Primo vantaggio:
+Posso andare a capo
+Secondo vantaggio:
+Posso "usare" i caratteri 'speciali' che mi \`pare\`
+Terzo vantaggio:
+Posso interpolare espressioni ${language.toUpperCase()}!`;
+console.log(templateLiteral);
+
+console.log(message);
+console.log(message[0], message[1], message[message.length - 1]);
+
+message[1] = "a";
+console.log(message);
+
+{
+  const newMessage = message[0] + "a" + message.slice(2);
+  console.log(message, newMessage);
+}
+
+{
+  const [firstChar, , ...rest] = message;
+  const newMessage = [firstChar, "a", ...rest].join("");
+  console.log(newMessage);
+}
